56ea88605a728e759355ce7eba105f5bf8737830
delila <delila@sinitic.ai>
2020-07-14 12:57:48 +0800

    Implement form in Contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,17 @@
 import React from "react"
+import { useForm } from "react-hook-form"
 
 export default function Contact() {
-  return <div>Contact page</div>
+  const { register, handleSubmit, watch, errors } = useForm()
+  const onSubmit = data => console.log(data)
+  console.log(watch("example"))
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input name="example" defaultValue="test" ref={register} />
+      <input name="exampleRequired" ref={register({ required: true })} />
+      {errors.exampleRequired && <span>This field is required!</span>}
+      <input type="submit" value="Submit" />
+    </form>
+  )
 }
