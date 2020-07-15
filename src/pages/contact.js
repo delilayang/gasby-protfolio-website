61d587e9a56122902a969f196a9363001e4d8cda
delila <delila@sinitic.ai>
2020-07-15 17:09:57 +0800

    Implement form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,8 +2,10 @@ import React from "react"
 import { useForm } from "react-hook-form"
 
 export default function Contact() {
-  const { register, handleSubmit, watch, errors } = useForm()
-  const onSubmit = formData => console.log(formData)
+  const { register, handleSubmit, watch, errors } = useForm({
+    reValidateMode: onSubmit,
+  })
+  const onSubmit = data => console.log(data)
   console.log(watch("example"))
 
   return (
@@ -12,30 +14,56 @@ export default function Contact() {
       onSubmit={handleSubmit(onSubmit)}
       className="contact-form"
     >
-      <label>Name:</label>
+      <label>Company:</label>
       <input
         type="text"
-        placeholder="name"
         name="nameRequired"
         ref={register({
           required: "Name required!",
-          minLength: { value: 3, message: "Name is invalid!" },
+          minLength: { value: 3, message: "Company name is invalid!" },
         })}
       />
       {errors.nameRequired && (
         <span className="error-message">{errors.nameRequired.message} </span>
       )}
 
+      <label>First name:</label>
+      <input
+        type="text"
+        name="firstNameRequired"
+        ref={register({
+          required: { value: true, message: "First name required!" },
+          maxLength: { value: 20, message: "First name is invalid!" },
+        })}
+      />
+      {errors.firstNameRequired && (
+        <span className="error-message">
+          {errors.firstNameRequired.message}
+        </span>
+      )}
+
+      <label>Last name:</label>
+      <input
+        name="lastNameRequired"
+        ref={register({
+          required: { value: true, message: "Last name required!" },
+          maxLength: { value: 20, message: "Last name is invalid!" },
+          pattern: { value: /^[A-Za-z]+$/i, message: "Last name is invalid!" },
+        })}
+      />
+      {errors.lastNameRequired && (
+        <span className="error-message">{errors.lastNameRequired.message}</span>
+      )}
+
       <label>Age:</label>
-      <input type="number" placeholder="age" name="age" />
+      <input name="age" type="number" ref={register({ min: 18, max: 99 })} />
 
       <label>Email:</label>
-      <input type="text" placeholder="email" name="email" ref={register} />
+      <input type="text" name="email" ref={register} />
 
       <label>Password:</label>
       <input
         type="text"
-        placeholder="password"
         name="passwordRequired"
         ref={register({
           required: "Password required!",
